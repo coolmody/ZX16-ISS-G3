77010f3810211818fc4f796b86488ed211ab7a35
mahmoud <mahmoud9304@aucegypt.edu>
2025-06-26 19:28:06 +0300

    Update cpu.ts

--- z16-sim/lib/cpu.ts
+++ z16-sim/lib/cpu.ts
@@ -82,7 +82,9 @@ export class cpu {
 
     const operation = instruction[0];
     switch (operation) {
+      //start of R-Type operations
       case "ADD": {
+        // ADD  rd ,rs  = rd =rd + rs
         const rd = binaryToDecimal(instruction[1], false);
         const rs = binaryToDecimal(instruction[2], false);
         this.registers[rd] = decimalToBinary(
@@ -92,7 +94,9 @@ export class cpu {
         );
         break;
       }
+      
       case "SUB": {
+        // SUB  rd ,rs  = rd =rd - rs
         const rd = binaryToDecimal(instruction[1], false);
         const rs = binaryToDecimal(instruction[2], false);
         this.registers[rd] = decimalToBinary(
@@ -103,18 +107,21 @@ export class cpu {
         break;
       }
       case "SLT": {
+        //ASK why isnt those 2 not set to true instead of false
+        // SLT rd, rs = rd = (rd < rs)  true/false (signed comparison)
         const rd = binaryToDecimal(instruction[1], false);
         const rs = binaryToDecimal(instruction[2], false);
         this.registers[rd] = decimalToBinary(
           binaryToDecimal(this.registers[rd], true) <
             binaryToDecimal(this.registers[rs], true)
-            ? 1
-            : 0,
+            ? 1  //true
+            : 0,  //false
           16
         );
         break;
       }
       case "SLTU": {
+        // SLTU rd, rs = rd = (rd < rs)  true/false (unsigned comparison)
         const rd = binaryToDecimal(instruction[1], false);
         const rs = binaryToDecimal(instruction[2], false);
         this.registers[rd] = decimalToBinary(
@@ -127,6 +134,8 @@ export class cpu {
         break;
       }
       case "SLL": {
+        // SLL logical shift left  rd= rd << rs 
+        // OR rd = rd shifted to the left by amount of rs
         const rd = binaryToDecimal(instruction[1], false);
         const rs = binaryToDecimal(instruction[2], false);
         this.registers[rd] = decimalToBinary(
@@ -137,6 +146,8 @@ export class cpu {
         break;
       }
       case "SRL": {
+        // SRL logical shift right  rd= rd >> rs
+        // OR rd = rd shifted to the right by amount of rs
         const rd = binaryToDecimal(instruction[1], false);
         const rs = binaryToDecimal(instruction[2], false);
         this.registers[rd] = decimalToBinary(
@@ -147,6 +158,8 @@ export class cpu {
         break;
       }
       //TODO: NOT WORKING
+      // SRA is arithmetic shift right, it should keep the sign bit
+      // SRA rd, rs = rd = rd >> rs (arithmetic shift right)
       case "SRA": {
         const rd = binaryToDecimal(instruction[1], false);
         const rs = binaryToDecimal(instruction[2], false);
@@ -155,9 +168,48 @@ export class cpu {
             (binaryToDecimal(this.registers[rs], true) & 15),
           16
         );
-        break;
+        // an attempt
+        //const rd = binaryToDecimal(instruction[1], false);
+        //const rs = binaryToDecimal(instruction[2], false);
+        //const x = this.registers[rd][8] === "1" ? 1 : 0; // check if the first bit is 1 (signed)
+        //if (x == 0){
+        //  
+        //      this.registers[rd] = decimalToBinary(
+        //  binaryToDecimal(this.registers[rd], true) >>
+        //    (binaryToDecimal(this.registers[rs], true) & 15),
+          //16
+      //  );
+        //else
+       //const rd_signed= binaryToDecimal(this.registers[rd], true);
+       //const shifts = binaryToDecimal(this.registers[rs], true) & 15;
+       // this.registers[rd] = decimalToBinary(value >> shift, 16);
+       //ATTEMPT 2 
+       //const rd = binaryToDecimal(instruction[1], false);
+        //const rs = binaryToDecimal(instruction[2], false);
+        //const x = 0
+        //if (this.registers[rd][8] === "1") { // check if the first bit is 1 (signed){
+        // while (x != rs){
+        ///this.registers[rd] = decimalToBinary(
+          //binaryToDecimal(this.registers[rd], true) >> 1
+           // 
+         //
+         //this.registers[rd] = decimalToBinary(
+          //binaryToDecimal(this.registers[rd], true) + 0x80 ,
+          //16
+        //);
+        //x++;
+      //}
+        //this.registers[rd] = decimalToBinary(
+          //binaryToDecimal(this.registers[rd], true) >>
+           // (binaryToDecimal(this.registers[rs], true) & 15),
+         // 16
+        //);
+
+       break;
       }
       case "OR": {
+        //bit wise operation OR
+        // OR rd, rs = rd = rd | rs
         const rd = binaryToDecimal(instruction[1], false);
         const rs = binaryToDecimal(instruction[2], false);
         this.registers[rd] = decimalToBinary(
@@ -168,6 +220,8 @@ export class cpu {
         break;
       }
       case "AND": {
+        // bit wise operation AND
+        // AND rd, rs = rd = rd & rs
         const rd = binaryToDecimal(instruction[1], false);
         const rs = binaryToDecimal(instruction[2], false);
         this.registers[rd] = decimalToBinary(
@@ -178,6 +232,8 @@ export class cpu {
         break;
       }
       case "XOR": {
+        // bit wise operation XOR
+        // XOR rd, rs = rd = rd ^ rs
         const rd = binaryToDecimal(instruction[1], false);
         const rs = binaryToDecimal(instruction[2], false);
         this.registers[rd] = decimalToBinary(
@@ -188,6 +244,8 @@ export class cpu {
         break;
       }
       case "MV": {
+        // MV not a core instruction but psudo instruction of ADD
+        // MV rd, rs = rd = rs
         const rd = binaryToDecimal(instruction[1], false);
         const rs = binaryToDecimal(instruction[2], false);
         this.registers[rd] = decimalToBinary(
@@ -197,17 +255,25 @@ export class cpu {
         break;
       }
       case "JR": {
+        // JR jump to address in register
+        // JR rd = PC = rd
+        // This instruction sets the program counter to the value in the specified register.
         const rd = binaryToDecimal(instruction[1], false);
         this.setPC(binaryToDecimal(this.registers[rd], false));
         break;
       }
       case "JALR": {
+        // JALR jump and link register
+        // JALR rd, rs = PC = rd; rd = PC + 2
         const rd = binaryToDecimal(instruction[1], false);
         const rs = binaryToDecimal(instruction[2], false);
         this.registers[rd] = decimalToBinary(this.getPC() + 2, 16);
         break;
       }
+      //end of R-Type operations
+      //start of I-Type operations 
       case "ADDI": {
+        // ADDI rd, imm = rd = rd + imm
         const rd = binaryToDecimal(instruction[1], false);
         const imm = binaryToDecimal(instruction[2], true);
         this.registers[rd] = decimalToBinary(
@@ -217,6 +283,8 @@ export class cpu {
         break;
       }
       case "SLTI": {
+        // SLTI rd, imm = rd = (rd < imm)  true/false (signed comparison)
+        //set less than immediate
         const rd = binaryToDecimal(instruction[1], false);
         const imm = binaryToDecimal(instruction[2], true);
         this.registers[rd] = decimalToBinary(
@@ -226,6 +294,9 @@ export class cpu {
         break;
       }
       case "SLTUI ": {
+        // SLTUI rd, imm = rd = (rd < imm)  true/false (unsigned comparison)
+        //set less than unsigned immediate
+        //sets rd with either 1 or 0 depending on the comparison
         const rd = binaryToDecimal(instruction[1], false);
         const imm = binaryToDecimal(instruction[2], false);
         this.registers[rd] = decimalToBinary(
@@ -235,6 +306,8 @@ export class cpu {
         break;
       }
       case "SLLI": {
+        // SLLI rd, imm = rd = rd << imm
+        //logical shift left immediate
         const rd = binaryToDecimal(instruction[1], false);
         const imm = binaryToDecimal(instruction[2], false);
         this.registers[rd] = decimalToBinary(
@@ -244,6 +317,8 @@ export class cpu {
         break;
       }
       case "SRLI": {
+        //  SRLI rd, imm = rd = rd >> imm
+        //logical shift right immediate
         const rd = binaryToDecimal(instruction[1], false);
         const imm = binaryToDecimal(instruction[2], false);
         this.registers[rd] = decimalToBinary(
@@ -254,6 +329,8 @@ export class cpu {
       }
       //TODO: NOT WORKING
       case "SRAI": {
+        // SRAI rd, imm = rd = rd >> imm (arithmetic shift right immediate)
+        // This instruction performs an arithmetic right shift on the value in rd by the amount specified in imm.
         const rd = binaryToDecimal(instruction[1], false);
         const imm = binaryToDecimal(instruction[2], false);
         this.registers[rd] = decimalToBinary(
@@ -261,8 +338,22 @@ export class cpu {
           16
         );
         break;
+        // SRAI attempt 1
+        //const rd = binaryToDecimal(instruction[1], false);
+        //const imm = binaryToDecimal(instruction[2], false);
+        //const x= this.registers[rd][0] === "1" ? 1 : 0; // check if the first bit is 1 (signed)
+        //this.registers[rd] = decimalToBinary(
+        //  binaryToDecimal(this.registers[rd], true) >> imm,
+        //  16
+        //);
+        // ATTEMPT 2
+        //const rd = binaryToDecimal(instruction[1], false);
+        //const imm = binaryToDecimal(instruction[2], false);
+        //const x=0
       }
       case "ORI": {
+        // ORI rd, imm = rd = rd | imm
+        //bit wise operation OR immediate
         const rd = binaryToDecimal(instruction[1], false);
         const imm = binaryToDecimal(instruction[2], true);
         this.registers[rd] = decimalToBinary(
@@ -272,6 +363,8 @@ export class cpu {
         break;
       }
       case "ANDI": {
+        // ANDI rd, imm = rd = rd & imm
+        //bit wise operation AND immediate
         const rd = binaryToDecimal(instruction[1], false);
         const imm = binaryToDecimal(instruction[2], true);
         this.registers[rd] = decimalToBinary(
@@ -281,6 +374,8 @@ export class cpu {
         break;
       }
       case "XORI": {
+        // XORI rd, imm = rd = rd ^ imm
+        //bit wise operation XOR immediate
         const rd = binaryToDecimal(instruction[1], false);
         const imm = binaryToDecimal(instruction[2], true);
         this.registers[rd] = decimalToBinary(
@@ -290,12 +385,16 @@ export class cpu {
         break;
       }
       case "LI": {
+        // LI rd, imm = rd = imm
+        // Load immediate value into register
         const rd = binaryToDecimal(instruction[1], false);
         const imm = binaryToDecimal(instruction[2], true);
         this.registers[rd] = decimalToBinary(imm, 16);
         break;
       }
       case "J": {
+        // J jump to address
+        // J imm = PC = imm/2
         const imm = binaryToDecimal(instruction[1], true);
         this.setPC(this.getPC() + imm / 2);
         return;
